edf59e70921f7f74a62bb18c252f57189c9ff4c1
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-25 21:00:43 +0000

    Create grid and dont draw last space

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ cellSize = 100
 
 function love.load()
     setup()
+    createGrid()
 end
 
 function love.update()
@@ -18,23 +19,35 @@ function setup()
     love.graphics.setNewFont(36)
 end
 
+function createGrid()
+    grid = {}
+    for x = 1, gridSizeX do
+        grid[x] = {}
+        for y = 1, gridSizeY do
+            grid[x][y] = x + ((y - 1) * gridSizeX)
+        end
+    end
+end
+
 function drawTiles()
     for x = 1, gridSizeX do
         for y = 1, gridSizeY do
-            love.graphics.setColor(0, 191, 191)
-            love.graphics.rectangle(
-                'fill',
-                (x - 1) * cellSize,
-                (y - 1) * cellSize,
-                cellSize - 1,
-                cellSize - 1
-            )
-            love.graphics.setColor(255, 255, 255)
-            love.graphics.print(
-                x + ((y - 1) * 4),
-                (x - 1) * cellSize + 5,
-                (y - 1) * cellSize + 5
-            )
+            if grid[x][y] ~= gridSizeX * gridSizeY then
+                love.graphics.setColor(0, 191, 191)
+                love.graphics.rectangle(
+                    'fill',
+                    (x - 1) * cellSize,
+                    (y - 1) * cellSize,
+                    cellSize - 1,
+                    cellSize - 1
+                )
+                love.graphics.setColor(255, 255, 255)
+                love.graphics.print(
+                    grid[x][y],
+                    (x - 1) * cellSize + 5,
+                    (y - 1) * cellSize + 5
+                )
+            end
         end
     end
 end
