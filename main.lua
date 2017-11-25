bb8d5e50c21b565cc531fa7c15de32a6af2fa2cc
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-25 21:49:28 +0000

    Shuffle board on game load

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 gridSizeX = 4
 gridSizeY = 4
 cellSize = 100
+numShuffles = 1000
 
 function love.load()
     setup()
     createGrid()
+    shuffleTiles(numShuffles)
 end
 
 function love.update()
@@ -16,6 +18,39 @@ function love.draw()
 end
 
 function love.keypressed(key)
+    moveDirection(key)
+end
+
+function setup()
+    love.graphics.setNewFont(36)
+end
+
+function createGrid()
+    grid = {}
+    for x = 1, gridSizeX do
+        grid[x] = {}
+        for y = 1, gridSizeY do
+            grid[x][y] = x + ((y - 1) * gridSizeX)
+        end
+    end
+end
+
+function shuffleTiles(numShuffles)
+    for moveNumber=1, numShuffles do
+        local roll = love.math.random(4)
+        if roll == 1 then
+            moveDirection('right')
+        elseif roll == 2 then
+            moveDirection('up')
+        elseif roll == 3 then
+            moveDirection('left')
+        elseif roll == 4 then
+            moveDirection('down')
+        end
+    end
+end
+
+function moveDirection(direction)
     local emptyX
     local emptyY
 
@@ -31,33 +66,19 @@ function love.keypressed(key)
     local newEmptyX = emptyX
     local newEmptyY = emptyY
 
-    if key == 'right' then
+    if direction == 'right' then
         newEmptyX = emptyX + 1
-    elseif key == 'up' then
+    elseif direction == 'up' then
         newEmptyY = emptyY - 1
-    elseif key == 'left' then
+    elseif direction == 'left' then
         newEmptyX = emptyX - 1
-    elseif key == 'down' then
+    elseif direction == 'down' then
         newEmptyY = emptyY + 1
     end
 
     if grid[newEmptyX] and grid[newEmptyX][newEmptyY] then
         grid[newEmptyX][newEmptyY], grid[emptyX][emptyY] = 
             grid[emptyX][emptyY], grid[newEmptyX][newEmptyY]
-    end
-end
-
-function setup()
-    love.graphics.setNewFont(36)
-end
-
-function createGrid()
-    grid = {}
-    for x = 1, gridSizeX do
-        grid[x] = {}
-        for y = 1, gridSizeY do
-            grid[x][y] = x + ((y - 1) * gridSizeX)
-        end
     end
 end
 
